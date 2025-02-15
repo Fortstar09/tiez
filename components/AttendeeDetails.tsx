f36cd2740fbb03a3a2ae1d9a15af78e89f713205
Fortstar09 <ogunyafort@gmail.com>
2025-02-15 01:37:54 +0100

    fix upload issue

--- components/AttendeeDetails.tsx
+++ components/AttendeeDetails.tsx
@@ -1,7 +1,10 @@
-import React from "react";
-import TicketTitle from "./TicketTitle";
+import React, { useCallback, useState } from "react";
 import StepTitle from "./StepTitle";
 import { PersonalInfo } from "./PersonalInfo";
+import { CloudDownload } from "lucide-react";
+import { useDropzone } from "react-dropzone";
+import Image from "next/image";
+// import { Image } from "cloudinary-react";
 
 
 interface AttendeeDetailsProps {
@@ -10,12 +13,66 @@ interface AttendeeDetailsProps {
 }
 
 const AttendeeDetails: React.FC<AttendeeDetailsProps> = ({ step, setStep }) => {
+
+    const [uploadedFiles, setUploadedFiles] = useState([]);
+
+    const onDrop = useCallback((acceptedFiles: File[]) => {
+        const url = `https://api.cloudinary.com/v1_1/$dsns1khez/upload`;
+    
+        acceptedFiles.forEach(async (acceptedFile: File) => {
+        //   const { signature, timestamp } = await getSignature();
+    
+          const formData = new FormData();
+          formData.append("file", acceptedFile);
+          formData.append(
+             "upload_preset",
+             "ml_default"
+          );
+          formData.append("api_key", "595733815843823");
+    
+          const response = await fetch(url, {
+            method: "post",
+            body: formData,
+          });
+          const data = await response.json();
+            setUploadedFiles(data.url);
+            console.log(data);
+            console.log(data.url);
+        });
+      }, []);
+    
+      const { getRootProps, getInputProps, isDragActive } = useDropzone({
+        onDrop,
+        accept: { "image/*": [] },
+        multiple: false,
+      });
+
     return (
         <div className="flex justify-between flex-col gap-8">
             <StepTitle step={step} />
             <div className="p-6 border-1 border border-stroke rounded-[32px] w-full">
                 <div className="flex flex-col gap-8">
-                    <TicketTitle />
+                    <div className="flex flex-col justify-center gap-8 px-6 pt-6 pb-12 rounded-3xl bg-[#052228] border border-[#07373F]">
+                        <h2 className="text-white text-base roboto leading-normal">Upload Picture</h2>
+                        <div className="relative w-[508] h-[200px] bg-[#000000]/20">
+                            {uploadedFiles ? (
+                                <div {...getRootProps()} className={`${ isDragActive ? "border-[#24A0B5]" : "border-[#24A0B5]/50"} absolute -bottom-5 right-32 flex flex-col items-center justify-center gap-4 px-6 py-[72px] border-4 bg-[#0E464F] rounded-[30px]`}>
+                                <input  {...getInputProps()}/>
+                                <CloudDownload className="text-white" />
+                                <p className="max-w-[192px] text-white text-center text-base roboto leading-normal">Drag & drop or click to upload</p>
+                            </div>
+                            ): (
+                                <Image
+                                src={uploadedFiles}
+                                        width={240}
+                                        height={240}
+                                        alt="avatar"
+                                        className={`border-[#24A0B5]/50 absolute -bottom-5 right-32 flex flex-col items-center justify-center gap-4 px-6 py-[72px] border-4 bg-[#0E464F] rounded-[30px]`} />
+                        
+                            )}
+                            
+                        </div>
+                    </div>
                     <div className="bg-[#0E464F] w-full h-1"></div>
                     <PersonalInfo setStep={setStep} />
                     {/* <pre>{JSON.stringify(ticketData, null, 2)}</pre> */}
